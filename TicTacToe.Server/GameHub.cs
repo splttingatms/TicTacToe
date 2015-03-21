f3090a72173ba579d86e4ff509aed104e5a4cba4
splttingatms <splttingatms@gmail.com>
2015-03-21 12:12:36 -0700

    Added SignalR to client side and startup connection.

---
+++ TicTacToe.Server/GameHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Server
+{
+    public class GameHub : Hub
+    {
+        public async Task FindGame(string username)
+        {
+            await Task.Factory.StartNew(() => { });
+        }
+    }
+}
