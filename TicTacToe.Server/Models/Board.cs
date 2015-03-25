2cf8778dad888dd47f6649df388b798cfce8bf43
splttingatms <splttingatms@gmail.com>
2015-03-25 00:29:29 -0700

    Include new board class.

---
+++ TicTacToe.Server/Models/Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Server.Models
+{
+    /// <summary>
+    /// Represents a Tic-Tac-Toe board and where players have placed their pieces.
+    /// </summary>
+    public class Board
+    {
+        public string[,] Pieces { get; private set; }
+
+        public Board()
+        {
+            this.Pieces = new string[3, 3];
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", this.Pieces);
+        }
+    }
+}
